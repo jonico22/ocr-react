bb3b56d9e464e9d94b930c8751d8d75fd732c79b
Jose Luis <joseyananicolas@gmail.com>
2020-02-16 21:34:33 -0500

    implementacion tesseract.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
 import './App.css';
+import img from "./img.jpeg"
 
 function App() {
+  const worker = createWorker({
+    logger: m => console.log(m),
+  });
+  const doOCR = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } =await worker.recognize(img);
+    let arr = text.split(" ")
+    console.log(arr)
+    setOcr(text);
+    await worker.terminate();
+  };
+  
+  const [ocr, setOcr] = useState('Recognizing...');
+  useEffect(() => {
+    doOCR();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{ocr}</p>
     </div>
   );
 }
 
 export default App;
+
